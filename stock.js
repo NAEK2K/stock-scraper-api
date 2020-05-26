9b12674da1cf29358f093bbdcd9362a44e5598db
NAEK2K <naek2k@outlook.com>
2020-05-26 06:33:35 -0400

    added express for api access

--- stock.js
+++ stock.js
@@ -1,6 +1,10 @@
 const got = require('got');
 const cheerio = require('cheerio');
 
+const express = require('express');
+const app = express();
+const port = 8080
+
 const scrape = (async (ticker) => {
     try {
         let jsonRes = {}
@@ -8,14 +12,25 @@ const scrape = (async (ticker) => {
         const $ = cheerio.load(response.body);
         let price = $('span.price span').text();
         let change = $('strong.text-green').text().replace(/\s/g, '').split('(').map((x) => x.replace(')', '').replace('%', ''));
-        if(change.length != 2) change = $('strong.text-red').text().replace(/\s/g, '').split('(').map((x) => x.replace(')', '').replace('%', ''));
+        if (change.length != 2) change = $('strong.text-red').text().replace(/\s/g, '').split('(').map((x) => x.replace(')', '').replace('%', ''));
         jsonRes.ticker = ticker;
         jsonRes.value = price;
         jsonRes.changeValue = change[0];
         jsonRes.changePercent = change[1];
-        jsonRes.positiveChange = !jsonRes.changeValue.includes('-');
+        // jsonRes.positiveChange = !jsonRes.changeValue.includes('-');
+        jsonRes.positiveChange = jsonRes.changeValue > 0
         return jsonRes
     } catch (e) {
         console.log(error.response.body);
     };
 });
+
+
+
+app.get('/ticker/:ticker', (req, res) => {
+    scrape(req.params.ticker).then((results) => {
+        res.send(results)
+    })
+})
+
+app.listen(port)
